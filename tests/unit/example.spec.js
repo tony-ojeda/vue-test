b65adb6430d4351cd338e6347dc7bebc410386df
Tony Ojeda <tojeda96@gmail.com>
2020-11-20 06:34:55 -0500

    feature: test with jest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mount} from '@vue/test-utils'
+import App from '@/App.vue'
+import {huevo, espinaca} from "./jestTest"
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+const wrapper = mount(App)
+const vm = wrapper.vm
+
+describe('Se probara la calidad de los productos', ()=>{
+	test('Calidad del huevo',() => {
+		expect(huevo().estado[0]).toBe('fresco')
+	});
+	
+	test('Calidad de la espinaca', () => {
+		expect(espinaca()).toEqual({
+			estado: 1,
+			color: 'verde'
+		})
+	})
 })
+
+// console.log('data: ',vm.$data,'props',vm.$props,'el',vm.$el,'children',vm.$children)
---
+++ tests/unit/jestTest.js
@@ -0,0 +1,19 @@
+function huevo() {
+	return {
+		estado: ['fresco'],
+		color: '#000'
+	}
+}
+
+function espinaca(){
+	return {
+		estado: 1,
+		color: "verde"
+	}
+}
+
+
+module.exports = {
+	huevo,
+	espinaca
+};
